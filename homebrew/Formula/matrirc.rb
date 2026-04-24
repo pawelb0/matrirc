a85f3b3f415891d2f902c3777f59e6e564ec9ac0
pawelb0 <pawelb0@users.noreply.github.com>
2026-04-24 07:49:39 -0700

    Pin formula sha256 for v0.1.0

--- homebrew/Formula/matrirc.rb
+++ homebrew/Formula/matrirc.rb
@@ -2,7 +2,7 @@ class Matrirc < Formula
   desc "Local IRC server backed by Matrix"
   homepage "https://github.com/pawelb0/matrirc"
   url "https://github.com/pawelb0/matrirc/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "REPLACE_WITH_sha256_OF_TARBALL"
+  sha256 "01a5f8981a1236cca8e9035a9bb376e06be36f586d9294459b78b4c90279c918"
   license "GPL-3.0-or-later"
   head "https://github.com/pawelb0/matrirc.git", branch: "main"
 
