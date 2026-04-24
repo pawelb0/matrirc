f1d3719f80bc74d953a5b4e5d276722b8b8cc5d3
pawelb0 <pawelb0@users.noreply.github.com>
2026-04-24 09:30:59 -0700

    Pin formula to v0.1.2

--- homebrew/Formula/matrirc.rb
+++ homebrew/Formula/matrirc.rb
@@ -1,8 +1,8 @@
 class Matrirc < Formula
   desc "Local IRC server backed by Matrix"
   homepage "https://github.com/pawelb0/matrirc"
-  url "https://github.com/pawelb0/matrirc/archive/refs/tags/v0.1.1.tar.gz"
-  sha256 "8c4f5a774cd44dfe6e29ff0fa4b4ba98b8f8b82106ed0867831b793321b3b049"
+  url "https://github.com/pawelb0/matrirc/archive/refs/tags/v0.1.2.tar.gz"
+  sha256 "50f988c5bbd80f020b42b2e0c4f40bc730e690415540d2fda207b81bcd23c70d"
   license "GPL-3.0-or-later"
   head "https://github.com/pawelb0/matrirc.git", branch: "main"
 
