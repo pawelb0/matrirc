eeedc65283a3c0e1d8a878a21f22081917a9e231
pawelb0 <pawelb0@users.noreply.github.com>
2026-04-25 17:45:24 -0700

    homebrew: v0.2.5

--- homebrew/Formula/matrirc.rb
+++ homebrew/Formula/matrirc.rb
@@ -1,8 +1,8 @@
 class Matrirc < Formula
   desc "Local IRC server backed by Matrix"
   homepage "https://github.com/pawelb0/matrirc"
-  url "https://github.com/pawelb0/matrirc/archive/refs/tags/v0.2.4.tar.gz"
-  sha256 "fcf2dbba0682d7f73d7968c825ff34cdb43565957aee2ea27f39ede647cf5b2c"
+  url "https://github.com/pawelb0/matrirc/archive/refs/tags/v0.2.5.tar.gz"
+  sha256 "fd13d92cde3fa1a73ff68b7b509966072a3f5226776083e16426bf2c232c1725"
   license "GPL-3.0-or-later"
   head "https://github.com/pawelb0/matrirc.git", branch: "main"
 
